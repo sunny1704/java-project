4dd676931d43f15793834e122ecefaca20563775
root <root@ip-172-31-31-231.us-west-2.compute.internal>
2017-12-24 03:03:11 +0000

    added test files

---
+++ src/RectangleTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static.org.junit.Assert.assertEquals;
+
+public class RectangleTest {
+	Rectangle myRect = new Rectangle(5,6);
+
+	@Test
+	public void getTestArea() {
+		assertEquals(myRect.getArea(), 30);
+	}
+}
