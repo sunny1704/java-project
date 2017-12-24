429963ca5ad959e26ce8466c15d8c2c3a7efea43
root <root@ip-172-31-31-231.us-west-2.compute.internal>
2017-12-24 03:11:28 +0000

    fixed code

--- src/RectangleTest.java
+++ src/RectangleTest.java
@@ -1,5 +1,5 @@
 import org.junit.Test;
-import static.org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class RectangleTest {
 	Rectangle myRect = new Rectangle(5,6);
